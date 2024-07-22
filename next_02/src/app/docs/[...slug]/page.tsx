2c2024b091642d2276dd8d943a6641655c0d4661
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-07-22 06:54:16 +0545

    init

--- next_02/src/app/docs/[...slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const docs = ({ params }: { params: { slug: string[] } }) => {
-  return <div>docs</div>;
-};
-
-export default docs;
---
+++ next_02/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <>
+      <h1>Page not found</h1>
+      <h2>Could not find requested resource</h2>
+    </>
+  );
+};
+
+export default notFound;

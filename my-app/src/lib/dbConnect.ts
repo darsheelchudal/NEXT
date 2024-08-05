9d6616330c64cc6f561861874266de16b4f2d95c
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-08-05 20:20:45 +0545

    db connections and env config

---
+++ my-app/src/lib/dbConnect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number; // ?: means ho bhi sakti hai nahi bhi hosakti hai
+};
+
+const connection: connectionObject = {};
+
+const dbConnect = async (): Promise<void> => {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+    console.log(db);
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Db connected successfully");
+  } catch (err) {
+    console.log("Database connection failed", err);
+    process.exit();
+  }
+};
+
+export default dbConnect;
